5dcb16917f54b41a282d8727f30d216fd10acc0f
Supawadee <Supawadee018@gmail.com>
2019-11-14 09:06:58 +0700

    Start 14 Nov

--- lib/widget/control.dart
+++ lib/widget/control.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
+
 class Control extends StatefulWidget {
   @override
   _ControlState createState() => _ControlState();
 }
 
 class _ControlState extends State<Control> {
+  //Field
+  bool modeBool = false;
+
+  //Method
+
+  Widget switchMode() {
+    return Container(
+      width: 100.0,
+      child: Switch(
+        value: modeBool,
+        onChanged: (bool value) {
+          changeBool(value);
+        },
+      ),
+    );
+  }
+
+  void changeBool(bool value) {
+    setState(() {
+      modeBool = value;
+      print('modeBool=$modeBool');
+    });
+  }
+
+  Widget topRow() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[switchMode(), switchMode()],
+    );
+  }
+
+  Widget bottonRow() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[switchMode(), switchMode()],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('This is Control'),
-      
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[topRow(), bottonRow()],
+      ),
     );
   }
 }
--- lib/widget/monitor.dart
+++ lib/widget/monitor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:webview_flutter/webview_flutter.dart';
 
 class Monitor extends StatefulWidget {
   @override
@@ -6,10 +7,38 @@ class Monitor extends StatefulWidget {
 }
 
 class _MonitorState extends State<Monitor> {
+  /*
+  //Field
+  String gateTemp =
+      'https://thingspeak.com/channels/662286/widgets/93565?fbclid=IwAR3Gq2zONKLjqkD6ACcCJE8m5Z9d_kdW92kUBOFdAdYgyAGQYtVOWpi8bJo';
+
+  //Method
+
+  Widget showWebview(String url) {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      child: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted, //ข้อมูลมาจากtinkspake
+      ),
+    );
+  }
+
+  Widget showAllGade() {
+    return Container(
+      //padding: EdgeInsets.all(20.0),
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: <Widget>[showWebview(gateTemp)],
+      ),
+    );
+    
+  }*/
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('This is Monitor'),
-    );
+    return Center(child: Text('Monitor'),);
   }
 }
